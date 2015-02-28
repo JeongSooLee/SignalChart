dfff33f2a2d073badc654938c812a426cd087eef
JeongSoo Lee <jslee0118@gmail.com>
2015-02-28 16:00:54 +0900

    Initial Commit

---
+++ SignalChart/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  SignalChart
+//
+//  Created by 이정수 on 2015. 2. 28..
+//  Copyright (c) 2015년 mosition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SignalChart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SignalChart
+//
+//  Created by 이정수 on 2015. 2. 28..
+//  Copyright (c) 2015년 mosition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
